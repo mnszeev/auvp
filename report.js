00b8e511f543d74524bf3a039a86ec9b66839b07
mnszeev <marcelo.souza@zeev.it>
2026-01-06 17:22:14 -0300

    Refactor report.js for improved data source handling
    Refactor data source definitions and update async functions for better structure and readability.

--- report.js
+++ report.js
@@ -11,29 +11,28 @@ Zeev.Controller = {
     MapDataSources: () => {
       Zeev.Integration.Functions.AddDataSource("VehicleCompensationByReferenceMonth", {
         name: "Indenização de Veículos Por Mês de Referência",
-       // name: "Indenização de Veículos Por Mês de Referência - Te",
-        Dev: "/api/2.0/integrations/595e7171-4722-4075-9471-7390caef45c2/execute",
-        Hml: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJsL789kHjzp988GeZr-A7ANpUIoXnJrpXW3c6fbH5dU1nJBoSt-YQVBZXhqrCsqEFg__",
+        Dev: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJsL789kHjzp988GeZr-A7ANpUIoXnJrpXW3c6fbH5dU1nJBoSt-YQVBZXhqrCsqEFg__",
         Prd: "/api/2.0/integrations/6b77ba2f-aa98-468b-9301-727a4c516403/execute",
-        //Prd: "https://zeev.sefaz.rs.gov.br/api/2.0/integrations/e2653fff-354a-4dda-8b18-7a22d2bb87da/execute",
       });
       Zeev.Integration.Functions.AddDataSource("CitiesOfRS", {
         name: "Cidades do Rio Grande do Sul",
-        Dev: "/api/2.0/integrations/b0d1a3d3-6dac-4d81-b81a-770bc7914086/execute",
-        Hml: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJm3KpIkO9CEecRV7w2QK8zeHVeTsxPHSG6T8E9JuFOZQxQYJqh4LLNpthHEON9-d8Q__",
+        Dev: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJm3KpIkO9CEecRV7w2QK8zeHVeTsxPHSG6T8E9JuFOZQxQYJqh4LLNpthHEON9-d8Q__",
         Prd: "/api/2.0/integrations/0c434beb-b11f-4c0c-8aac-032b2d349c10/execute",
       });
       Zeev.Integration.Functions.AddDataSource("ReleaseCompensationBatchReport", {
         name: "Report Liberação Lote de Indenização",
-        Dev: "/api/2.0/integrations/21fa0ef6-23cc-449d-b698-91ba417aacb0/execute",
-        Hml: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJqzCo3gJTKEkcMpVpdD7z9e8VIDTM7@lc-CF4Ek3UGbNViqHl46mioX@gvJVKBB6tg__",
-        Prd: "/api/2.0/integrations/5dce0d79-b0b1-4aac-9de9-70552451b7f0/execute",
+        Dev: "/api/internal/legacy/1.0/datasource/get/1.0/qw0Xk6xWKL563BI8VvBqJqzCo3gJTKEkcMpVpdD7z9e8VIDTM7@lc-CF4Ek3UGbNViqHl46mioX@gvJVKBB6tg__",
+        Dev: "/api/2.0/integrations/5dce0d79-b0b1-4aac-9de9-70552451b7f0/execute",
       });
     },
-    Init: () => {
+    Init: async () => {
+      Zeev.Resources.CustomVariables.Environments = "Prd";
+      Zeev.System.IsDebug = true;
+
       Zeev.Resources.Functions.MapNativeResources();
       Zeev.Controller.Settings.MapFields();
       Zeev.Controller.Settings.MapDataSources();
+      await Zeev.Controller.CustomerRules.GenerateCsv();
     },
   },
   CustomerRules: {
@@ -59,53 +58,13 @@ Zeev.Controller = {
       const citiesOfRS = await Zeev.Controller.CustomerRules.FetchGetRequest("CitiesOfRS");
       return [].concat(citiesOfRS || []);
     },
-
-    sleep: async (ms) => {
-      return new Promise((resolve) => setTimeout(resolve, ms));
-    },
-
-    MountVehicleCompensationPostBody: (referenceMonth, pageNumber) => {
-      const requestBody = {
-        startDateIntervalBegin: "2024-01-01T00:00:00",
-        startDateIntervalEnd: "2028-01-01T00:00:00",
-        simulation: false,
-        flowId: 112,
-        pageNumber: pageNumber,
-        recordsPerPage: 100,
-        formFieldNames: [
-          "servidor",
-          "municipio",
-          "valor_tarifa_km",
-          "km_maxima_sem_comprovacao",
-          "aRT6OR",
-          "art8OR",
-          "art9OUnicoR",
-          "totalKmPercorrida",
-          "codigo_credor",
-          "totalR",
-          "area",
-        ],
-        formFieldsFilter: [
-          {
-            name: "mes_referencia",
-            operator: "=",
-            value: referenceMonth,
-          },
-        ],
-      };
-
-      return JSON.stringify(requestBody);
-    },
-
     GetVehicleCompensationByReferenceMonth: async () => {
-      const referenceMonth = document.getElementById("inpmes_referencia").value;
+      const referenceMonth = document.querySelector("[xid='divmes_referencia']").innerHTML;
+
       const allResults = [];
-
-      for (let pageNumber = 1;; pageNumber++) {
-        const params = {
-          inpbody: Zeev.Controller.CustomerRules.MountVehicleCompensationPostBody(referenceMonth, pageNumber),
-        };
-
+      for (let pageNumber = 1; pageNumber <= 20; pageNumber++) {
+        let params = { inpmesReferencia: referenceMonth, inppageNumberAtual: pageNumber, recordsPerPage: 100 };
+        console.log(params);
         const response = await Zeev.Controller.CustomerRules.FetchPostRequest(
           "VehicleCompensationByReferenceMonth",
           params
@@ -114,7 +73,7 @@ Zeev.Controller = {
         const data = Array.isArray(response) ? response : response?.items || response?.data || [];
 
         if (!data || data.length === 0) {
-          break; // encerra quando não houver mais páginas
+          break;
         }
 
         allResults.push(...data);
@@ -122,11 +81,11 @@ Zeev.Controller = {
 
       return allResults;
     },
-
     GenerateCsv: async () => {
       const citiesOfRS = await Zeev.Controller.CustomerRules.GetCitiesOfRS();
       const vehicleCompensationByReferenceMonth =
         await Zeev.Controller.CustomerRules.GetVehicleCompensationByReferenceMonth();
+
       Zeev.Controller.CustomerRules.GenerateCsvFile("STI", vehicleCompensationByReferenceMonth, citiesOfRS);
       Zeev.Controller.CustomerRules.GenerateCsvFile("DEPAD", vehicleCompensationByReferenceMonth, citiesOfRS);
       Zeev.Controller.CustomerRules.GenerateCsvFile("GSF", vehicleCompensationByReferenceMonth, citiesOfRS);
@@ -135,22 +94,26 @@ Zeev.Controller = {
       Zeev.Controller.CustomerRules.GenerateCsvFile("Tesouro", vehicleCompensationByReferenceMonth, citiesOfRS);
       Zeev.Controller.CustomerRules.GenerateCsvFile("Servidores", vehicleCompensationByReferenceMonth, citiesOfRS);
     },
-    GenerateCsvFile: (area, servantList, cityList) => {
-      let csvRows = "";
-      let committedValue = 0;
-      let filteredServantList;
 
+    FilterByArea: (servantList, area) => {
       if (area === "Servidores") {
-        filteredServantList = servantList = servantList.filter(
+        return (servantList = servantList.filter(
           (item) => item.fields.flowResult === "Enviado para aprovação de lote"
-        );
+        ));
       } else {
-        filteredServantList = servantList = servantList.filter(
+        return (servantList = servantList.filter(
           (item) =>
             item.fields.area.toUpperCase() === area.toUpperCase() &&
             item.fields.flowResult === "Enviado para aprovação de lote"
-        );
+        ));
       }
+    },
+    GenerateCsvFile: (area, servantList, cityList) => {
+      let csvRows = "";
+      let committedValue = 0;
+
+      const filteredServantList = Zeev.Controller.CustomerRules.FilterByArea(servantList, area);
+
       for (const servant of filteredServantList) {
         const name = servant.fields.nome;
         const codCredor = servant.fields.codigo_credor;
@@ -165,15 +128,11 @@ Zeev.Controller = {
         const totalValue = servant.fields.totalR;
         csvRows += `${name};${codCredor};${cityCode};${cityName};${fareValue};${kmMax};${art8Value};${kmTraveled};${art6Value};${art9Value};${totalValue};${kmMax}`;
         csvRows += `\n`;
-        const parsedTotal = Number(
-          totalValue.replace(/\./g, "").replace(",", ".")
-        );
-        committedValue += isNaN(parsedTotal) ? 0 : parsedTotal;
+        committedValue += Number(totalValue);
       }
 
-      const caseNumber = document.getElementById("inpcodigo_processo").value;
-      const competence = document.getElementById("inpmes_referencia").value;
-      committedValue = Number(committedValue.toFixed(2));
+      const caseNumber = document.querySelector("[xid='divcodigo_processo']").innerHTML;
+      const competence = document.querySelector("[xid='divmes_referencia']").innerHTML;
       let csvHeader = "";
       csvHeader += `Nome da área: ${area}`;
       csvHeader += `\n`;
@@ -216,62 +175,12 @@ Zeev.Controller = {
     },
     GenerateCsvElement: (csvElement, csvLink, csvName) => {
       const element = document.querySelector(`[xid='${csvElement}']`);
-      const uid = Zeev.Controller.CustomerRules.GeneratCsvId();
-      element.setAttribute("data-client-uid", uid);
       const html = `
                 <div class="containerFormFileLink">
                     <a href="${csvLink}" target="_blank" class="small">${csvName}</a>
-                    <a href="javascript:void(0)" class="ml-3" title="Excluir" onclick="javascript:execv2.form.fileUpload.delete('${uid}')">
-                        <svg class="ico-delete ico-sm text-danger" focusable="true"><use xlink:href="#delete"></use></svg>
-                    </a>
                 </div>`;
 
-      document.querySelector(`[xid='${csvElement}']`).innerHTML = html;
-    },
-    GeneratCsvId: () => {
-      const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      let result = "";
-      for (let i = 0; i < 9; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
-      return result;
-    },
-    FetchReleaseCompensationBatchReport: async () => {
-      const params = { inpflowId: FlowId };
-      const report = await Zeev.Controller.CustomerRules.FetchPostRequest("ReleaseCompensationBatchReport", params);
-
-      if (report) {
-        const lastInstance = Array.isArray(report) ? report[report.length - 1] : report;
-        Zeev.Form.Fields.DadosDoProcesso.Codigo_processo.SetValue(lastInstance.fields.codigo_processo);
-        Zeev.Form.Fields.DadosDoProcesso.Mes_referencia.SetValue(lastInstance.fields.mes_referencia);
-        Zeev.Form.Fields.DadosDoProcesso.Ano_indenizacao.SetValue(lastInstance.fields.ano_indenizacao);
-        Zeev.Form.Fields.DadosDoProcesso.Numero_dias_mes.SetValue(lastInstance.fields.numero_dias_mes);
-        Zeev.Form.Fields.DadosDoProcesso.Km_maxima_sem_comprovacao.SetValue(
-          lastInstance.fields.km_maxima_sem_comprovacao
-        );
-        Zeev.Form.Fields.DadosDoProcesso.Km_maxima_indenizavel.SetValue(lastInstance.fields.km_maxima_indenizavel);
-        Zeev.Form.Fields.DadosDoProcesso.Valor_tarifa_km.SetValue(lastInstance.fields.valor_tarifa_km);
-      }
-      const currentDate = Zeev.Controller.CustomerRules.GetFormatedCurrentDate();
-      Zeev.Form.Fields.DadosDoProcesso.Data_limite_ccuvp.SetValue(
-        Zeev.Controller.CustomerRules.CalculateSla(currentDate, 10)
-      );
-    },
-    GetFormatedCurrentDate() {
-      const now = new Date();
-      const day = String(now.getDate()).padStart(2, "0");
-      const month = String(now.getMonth() + 1).padStart(2, "0");
-      const year = now.getFullYear();
-      return `${day}/${month}/${year}`;
-    },
-    CalculateSla: (startDate, sla) => {
-      const [day, month, year] = startDate.split("/").map(Number);
-      const date = new Date(year, month - 1, day);
-      date.setDate(date.getDate() + sla);
-      const newDay = String(date.getDate()).padStart(2, "0");
-      const newMonth = String(date.getMonth() + 1).padStart(2, "0");
-      const newYear = date.getFullYear();
-      return `${newDay}/${newMonth}/${newYear}`;
+      element.innerHTML = html;
     },
   },
 };
